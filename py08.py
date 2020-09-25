3929376567a98049686865ee5454202cbdcd87b7
Jinie <71276587+yseonjin@users.noreply.github.com>
2020-09-26 01:17:37 +0900

    Add files via upload

---
+++ py08.py
@@ -0,0 +1,7 @@
+strdata1='I love'; strdata2='Python'; strdata3='you'
+listdata1=[1,2,3]; listdata2=[4,5,6]
+print(strdata1+strdata2)
+print(strdata1+strdata3*3)
+print(listdata1+listdata2)
+addata = '나와'+strdata3+'좋아한다'+strdata2*2+'!!'
+print(addata)
---
+++ py09.py
@@ -0,0 +1,8 @@
+listdata=[1,2,3,4]
+ret1=5 in listdata
+ret2=2 in listdata
+print(ret1);print(ret2)
+strdata='abcde'
+ret3='c' in strdata
+ret4='j' in strdata
+print(ret3);print(ret4)
